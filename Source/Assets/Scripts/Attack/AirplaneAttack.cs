300e4dc3af263f6a4f4d47ce93f56d46043b86a7
antony-jekov <antony.jekov@gmail.com>
2014-12-05 15:20:00 +0200

    Changed player shooting input.

--- Source/Assets/Scripts/Attack/AirplaneAttack.cs
+++ Source/Assets/Scripts/Attack/AirplaneAttack.cs
@@ -5,7 +5,7 @@ public class AirplaneAttack : BaseAttack
 {
     void Update()
     {
-        if (CanShoot() && Input.GetKeyUp(KeyCode.Space))
+        if (CanShoot() && Input.GetKey(KeyCode.Space))
         {
             Fire();
             Reload();
