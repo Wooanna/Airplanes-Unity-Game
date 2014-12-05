5f3d6e91b1b286b11e043ff4780c0c142b4d0d76
antony-jekov <antony.jekov@gmail.com>
2014-12-05 16:48:02 +0200

    Added armor logic and basic damage reduction.

--- Source/Assets/Scripts/Stats/AirplaneStats.cs
+++ Source/Assets/Scripts/Stats/AirplaneStats.cs
@@ -6,5 +6,6 @@ public class AirplaneStats : ElementStats {
     void OnGUI()
     {
         GUI.Box(new Rect(10, 10, Screen.width / 3, 25), Stats.Health + "/" + BaseStats.MaxHealth);
+        GUI.Box(new Rect(10, 45, Screen.width / 3, 25), armor + "/" + maxArmor);
     }
 }
--- Source/Assets/Scripts/Stats/ElementStats.cs
+++ Source/Assets/Scripts/Stats/ElementStats.cs
@@ -2,8 +2,11 @@
 using System.Collections;
 
 public class ElementStats : MonoBehaviour {
-
+    private const float MaxArmor = 200;
     public BaseStats Stats { get; set; }
+
+    public int maxArmor = 5;
+    public int armor = 5;
 
     public ElementStats()
     {
@@ -15,12 +18,40 @@ public class ElementStats : MonoBehaviour {
         Stats.AdjustHealth(amount);
     }
 
+    public void IncreaseArmor(int ammount)
+    {
+        this.maxArmor += ammount;
+        if (maxArmor > MaxArmor)
+        {
+            maxArmor = (int)MaxArmor;
+        }
+    }
+
+    public void RepairArmor(int amount)
+    {
+        this.armor += amount;
+        if (armor > maxArmor)
+        {
+            armor = maxArmor;
+        }
+    }
+
     public void InflictDamage(int amount)
     {
-        Stats.AdjustHealth(-amount); // TODO: add armor.
+        Stats.AdjustHealth(-(AdjustedDamage(amount)));
+        armor -= amount >> 3;
+        if (armor < 0)
+        {
+            armor = 0;
+        }
         if (Stats.Health <= 0)
         {
             Destroy(gameObject); // TODO: add explosion or something
         }
+    }
+
+    private int AdjustedDamage(int amount)
+    {
+        return (int)(amount * (1 - (armor / MaxArmor)));
     }
 }
