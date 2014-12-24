a3855cb5e26e61a1d0b71c2b49a102c2a4f70a07
Antony Jekov <antony.jekov@gmail.com>
2014-12-24 15:39:20 +0200

    Changed collision reaction.

---
+++ Source/Assets/Scripts/CollisionReaction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ElementStats))]
+public class CollisionReaction : MonoBehaviour {
+
+    ElementStats stats;
+
+	// Use this for initialization
+	void Awake () {
+        this.stats = GetComponent<ElementStats>();
+	}
+	
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            stats.InflictDamage(int.MaxValue);
+        }
+    }
+}
