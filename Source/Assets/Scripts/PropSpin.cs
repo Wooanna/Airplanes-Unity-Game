4875dda7a78121163a2f442366410a0199903187
Antony Jekov <antony.jekov@gmail.com>
2014-12-28 17:50:03 +0200

    Added props.

---
+++ Source/Assets/Scripts/PropSpin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropSpin : MonoBehaviour {
+
+    public float spinIntervalTime;
+
+    Material material;
+    WaitForSeconds spinInterval;
+    Vector2 offsetUpdate = new Vector2(.25f, 0);
+
+	// Use this for initialization
+	void Start () {
+        material = renderer.material;
+        this.spinInterval = new WaitForSeconds(spinIntervalTime);
+
+        StartCoroutine(SpinProp());
+	}
+	
+	IEnumerator SpinProp()
+    {
+        while (true)
+        {
+            yield return spinInterval;
+
+            material.mainTextureOffset += offsetUpdate;
+        }
+    }
+}
